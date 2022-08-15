20d452cc1db40bbc24092e077c412a3fc313606e
YurleidyAlcázar <yuralramfran@gmail.com>
2022-08-15 13:49:56 -0500

    Ejercicio 2

---
+++ Ejercicio2/src/ejercicio2/Ejercicio21.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio2;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Ejercicio21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        System.out.println("Se ejecuta este principal a pesar de que las otras dos clases tienen Main por lo tanto es posible tener varios Main en un mismo proyecto y elegir cual sera el principal");
+        
+    }
+    
+}
---
+++ Ejercicio2/src/ejercicio2/Ejercicio211.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio2;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Ejercicio211 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Este no se ejecutara porque no esta como clase principal a menos que sea llamado desde otra instancia");
+        
+    }
+    
+}
